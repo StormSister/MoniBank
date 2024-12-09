3ad3bede34844538a78794288aa54f910c076e60
StormSister <hydmet.import@gmail.com>
2024-12-09 19:22:58 +0100

    adding customer mapper

--- accounts/src/main/java/com/monika/accounts/repository/AccountRepository.java
+++ accounts/src/main/java/com/monika/accounts/repository/AccountRepository.java
@@ -1,9 +1,9 @@
 package com.monika.accounts.repository;
 
-import com.monika.accounts.entity.Account;
+import com.monika.accounts.entity.Accounts;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AccountRepository extends JpaRepository<Account, Long> {
+public interface AccountRepository extends JpaRepository<Accounts, Long> {
 }
