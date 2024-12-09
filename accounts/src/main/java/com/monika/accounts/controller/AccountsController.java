6e5c902557e179f695d767106df6e78437bfce0d
StormSister <hydmet.import@gmail.com>
2024-12-09 12:58:12 +0100

    change application.properties to application.yml, add schema for H2 database

---
+++ accounts/src/main/java/com/monika/accounts/controller/AccountsController.java
@@ -0,0 +1,13 @@
+package com.monika.accounts.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AccountsController {
+    @GetMapping("/sayHello")
+    public String hello(){
+        return "Hello World";
+    }
+
+}
