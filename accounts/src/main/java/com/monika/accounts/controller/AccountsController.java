54952b66b7bad5e147ddb83ec3260d910b058d33
StormSister <hydmet.import@gmail.com>
2024-12-09 18:50:43 +0100

    create constans class for accounts and constans package

--- accounts/src/main/java/com/monika/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/monika/accounts/controller/AccountsController.java
@@ -1,13 +1,26 @@
 package com.monika.accounts.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import com.monika.accounts.dto.AccountsDto;
+import com.monika.accounts.dto.CustomerDto;
+import com.monika.accounts.dto.ResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
 public class AccountsController {
-    @GetMapping("/sayHello")
-    public String hello(){
-        return "Hello World";
+    @PostMapping("/create")
+    public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto) {
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new ResponseDto());
+
     }
 
-}
+
+    }
