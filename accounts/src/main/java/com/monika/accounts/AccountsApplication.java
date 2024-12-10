4250e658c4e281ceae1d6543456d38be0bb328b4
StormSister <hydmet.import@gmail.com>
2024-12-10 17:00:53 +0100

    enchancing documentation of REST APIs using @OpenAPI

--- accounts/src/main/java/com/monika/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/monika/accounts/AccountsApplication.java
@@ -1,15 +1,38 @@
 package com.monika.accounts;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing(auditorAwareRef ="auditAwareImpl")
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(info = @Info(
+        title = "Accounts Microservice REST API Documentation",
+        version = "v1.0",
+        description = "MoniBank Accounts Microservice Rest API Documentation.",
+        contact = @Contact(
+                name = "Monika Gudalewska",
+                email = "monika@example.com",
+                url = "https://github.com/monikagudalewska/moni-bank-accounts"),
+        license = @License(
+                name = "Apache 2.0",
+                url = "http://www.apache.org/licenses/LICENSE-2.0.html"
+        )),
+        externalDocs = @ExternalDocumentation(
+                description = "MoniBank API Documentation",
+                url = "https://github.com/monikagudalewska/moni-bank-api"
+        )
+)
+
 public class AccountsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AccountsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AccountsApplication.class, args);
+    }
 
 }
