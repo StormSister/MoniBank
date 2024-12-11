951bb385417ae03fbeac988f56e0c263514d343c
StormSister <hydmet.import@gmail.com>
2024-12-11 20:27:40 +0100

    add loans dtos, global exception handler and custom exceptions for LoanAlreadyExists, ResourceNotFound Exceptions

--- loans/src/main/java/com/example/loans/controller/LoansController.java
+++ loans/src/main/java/com/example/loans/controller/LoansController.java
@@ -1,10 +1,32 @@
 package com.example.loans.controller;
 
+import com.example.loans.constans.LoansConstants;
+import com.example.loans.dto.ResponseDto;
+import com.example.loans.entity.Loans;
+import com.example.loans.service.ILoansService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
 
 @RestController
 @RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
 public class LoansController {
+
+    private ILoansService iLoansService;
+
+    @PostMapping("/create")
+    public ResponseEntity<ResponseDto> createLoan(@RequestParam String mobileNumber){
+        iLoansService.createLoan(mobileNumber);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(new ResponseDto(LoansConstants.STATUS_201, LoansConstants.MESSAGE_201));
+
+    }
+
+
 }
--- loans/src/main/java/com/example/loans/service/impl/LoansServiceImpl.java
+++ loans/src/main/java/com/example/loans/service/impl/LoansServiceImpl.java
@@ -1,4 +1,40 @@
 package com.example.loans.service.impl;
 
-public class LoansServiceImpl {
+import com.example.loans.constans.LoansConstants;
+import com.example.loans.entity.Loans;
+import com.example.loans.exception.LoanAlreadyExistsException;
+import com.example.loans.repository.LoansRepository;
+import com.example.loans.service.ILoansService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.Random;
+
+@Service
+@AllArgsConstructor
+public class LoansServiceImpl implements ILoansService {
+
+    private LoansRepository loansRepository;
+
+    @Override
+    public void createLoan(String mobileNumber) {
+        Optional<Loans> optionalLoans= loansRepository.findByMobileNumber(mobileNumber);
+        if(optionalLoans.isPresent()){
+            throw new LoanAlreadyExistsException("Loan already registered with given mobileNumber "+mobileNumber);
+        }
+        loansRepository.save(createNewLoan(mobileNumber));
+    }
+
+    private Loans createNewLoan(String mobileNumber) {
+        Loans newLoan = new Loans();
+        long randomLoanNumber = 100000000000L + new Random().nextInt(900000000);
+        newLoan.setLoanNumber(Long.toString(randomLoanNumber));
+        newLoan.setMobileNumber(mobileNumber);
+        newLoan.setLoanType(LoansConstants.HOME_LOAN);
+        newLoan.setTotalLoan(LoansConstants.NEW_LOAN_LIMIT);
+        newLoan.setAmountPaid(0);
+        newLoan.setOutstandingAmount(LoansConstants.NEW_LOAN_LIMIT);
+        return newLoan;
+    }
 }
