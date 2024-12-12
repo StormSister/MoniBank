6e48d5b52dcdf0cc8f0097f1e6f3d28ba7eb6298
StormSister <hydmet.import@gmail.com>
2024-12-12 09:30:50 +0100

    add documentation into main class in CardsAppliaction, enable auditing

--- cards/src/main/java/com/example/cards/CardsApplication.java
+++ cards/src/main/java/com/example/cards/CardsApplication.java
@@ -1,13 +1,38 @@
 package com.example.cards;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Cards microservice REST API Documentation",
+				description = "MoniBank Cards microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Madan Reddy",
+						email = "tutor@monibank.com",
+						url = "https://www.monibank.com"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.monika.com"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "MoniBank Cards microservice REST API Documentation",
+				url = "https://www.monika.com/swagger-ui.html"
+		)
+)
 public class CardsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CardsApplication.class, args);
 	}
-
 }
