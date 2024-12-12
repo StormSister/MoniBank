193ac94b8a8801121fa1b384680186b92e0ccbc6
StormSister <hydmet.import@gmail.com>
2024-12-12 09:21:14 +0100

    add GET, PUT, DELETE, POST methods to Cards Controller, driven methods to CardsRepository, implementations in CardsServiceImpl

--- cards/src/main/java/com/example/cards/repository/CardsRepository.java
+++ cards/src/main/java/com/example/cards/repository/CardsRepository.java
@@ -4,6 +4,14 @@ import com.example.cards.entity.Cards;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CardsRepository extends JpaRepository<Cards, Long> {
+
+    Optional<Cards> findByCardNumber(String cardNumber);
+
+    Optional<Cards> findByMobileNumber(String mobileNumber);
+
+
 }
