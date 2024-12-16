e20734f66c36a7ffe196dd2c29f37cf0b59517d2
StormSister <hydmet.import@gmail.com>
2024-12-16 11:53:39 +0100

    reading configurations using @ConfigurationProperties

--- accounts/src/main/java/com/monika/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/monika/accounts/controller/AccountsController.java
@@ -1,6 +1,7 @@
 package com.monika.accounts.controller;
 
 import com.monika.accounts.constans.AccountsConstants;
+import com.monika.accounts.dto.AccountsContactInfoDto;
 import com.monika.accounts.dto.CustomerDto;
 import com.monika.accounts.dto.ErrorResponseDto;
 import com.monika.accounts.dto.ResponseDto;
@@ -37,6 +38,9 @@ public class AccountsController {
 
     @Autowired
     private Environment environment;
+
+    @Autowired
+    private AccountsContactInfoDto accountsContactInfoDto;
 
     @Autowired
     public AccountsController(IAccountsService iAccountsService) {
@@ -214,6 +218,32 @@ public class AccountsController {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(environment.getProperty("JAVA_HOME"));
+    }
+
+
+    @Operation(
+            summary = "Get Contact Info",
+            description = "Contact info details that can be reached out in case of any issues"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
+    @GetMapping("/contact-info")
+    public ResponseEntity<AccountsContactInfoDto> getContactInfo() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(accountsContactInfoDto);
     }
 
 
